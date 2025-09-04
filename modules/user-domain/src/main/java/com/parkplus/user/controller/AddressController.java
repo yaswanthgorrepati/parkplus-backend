66abe9d6f19a847b27b4ea6afe37929143ce3e0b
yaswanth <gorrepatiyaswanth2@gmail.com>
2025-09-04 16:11:25 +0530

    address save 1:1 mapping

--- modules/user-domain/src/main/java/com/parkplus/user/controller/AddressController.java
+++ modules/user-domain/src/main/java/com/parkplus/user/controller/AddressController.java
@@ -5,11 +5,13 @@ import com.parkplus.user.repository.AddressRepository;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -29,8 +31,16 @@ public class AddressController {
     @PostMapping
     public Map<String, Object> create(Authentication auth, @RequestBody CreateAddressReq createAddressReq) {
         UUID uid = UUID.fromString(auth.getName());
-        Address address = new Address();
-        address.setId(UUID.randomUUID());
+        List<Address> addressList = addressRepository.findByUserId(uid);
+
+        Address address = null;
+        if(CollectionUtils.isEmpty(addressList)){
+            address = new Address();
+            address.setId(UUID.randomUUID());
+        }else {
+            address = addressList.get(0);
+        }
+
         address.setUserId(uid);
         address.setLabel(createAddressReq.label());
         address.setLine1(createAddressReq.line1());
